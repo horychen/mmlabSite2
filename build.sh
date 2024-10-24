933e8085fc44fc1c91de0dfe1b517f9ef96a1bf5
horychen <horychen@users.noreply.github.com>
2024-10-24 09:00:10 +0000

    deploy: fc35c75800ef4755357045fc892f341f14d08547

---
+++ build.sh
@@ -0,0 +1,4 @@
+cp "/mnt/c/DrH/[00]GetWorking/216 EE160-LectureNotes/ee160_2023_tex_overleaf/2023ee160.pdf" "/mnt/c/_Codes/mmlab-site6/mmlabSite2/content/courses/EE160/2023ee160.pdf"
+cp "/mnt/c/DrH/[00]GetWorking/205 EE275-LectureNotes/ee275_2023_TEX/2023ee275.pdf" "/mnt/c/_Codes/mmlab-site6/mmlabSite2/content/courses/EE275/2023ee275.pdf"
+"hugo.exe"
+cp -r "./rely_file/*" "./public/js/"
